31991aff438f290173528064e880ff465c0bbca8
maxim <demyanenkomaxim22@gmail.com>
2019-01-25 17:35:13 +0300

    some commit

---
+++ create_admin.py
@@ -0,0 +1,30 @@
+﻿from getpass import getpass
+import sys
+
+
+from webapp import create_app
+from webapp.db import db
+from webapp.news.models import User
+
+app = create_app()
+
+with app.app_context():
+    username = input('Enter name:')
+
+    if User.query.filter(User.username == username).count():
+        print('The user hve already exist')
+        sys.exit(0)
+
+    password1 = getpass('Enter password:')
+    password2 = getpass('Repeat password:')
+
+    if not password1 == password2:
+        print('passwords dont the same')
+        sys.exit(0)
+
+    new_user = User(username=username, role='admin')
+    new_user.set_password(password1)
+
+    db.session.add(new_user)
+    db.session.commit()
+    print('Created new user with id={}'.format(new_user.id))
---
+++ webapp/python_org_news.py
@@ -0,0 +1,40 @@
+﻿import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from webapp.db import db, News
+from webapp.news.models import News
+
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException, ValueError):
+        print("Net Error")
+        return False
+
+
+def get_python_news():
+    html = get_html("https://www.python.org/blogs/")
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
+        result_news = []
+        for news in all_news:
+            title = news.find('a').text
+            url = news.find('a')['href']
+            published = news.find('time').text
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
+            save_news(title=title, url=url, published=published)
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
